075901f63c5e9f35b8e0ada92820485be95f97c6
Piotr Mionskowski <piotr.mionskowski@gmail.com>
2018-06-14 08:58:17 +0200

    Js crypt expose in window

--- browser/scrypt.js
+++ browser/scrypt.js
@@ -462,3 +462,4 @@ var scrypt = (function () {
     on_ready(scrypt);
     return "scrypt_module_factory API has changed -- see js-scrypt README";
 });
+window.scrypt_module_factory = scrypt_module_factory;
--- scrypt_browser_suffix.js
+++ scrypt_browser_suffix.js
@@ -1,3 +1,4 @@
     on_ready(scrypt);
     return "scrypt_module_factory API has changed -- see js-scrypt README";
 });
+window.scrypt_module_factory = scrypt_module_factory;
